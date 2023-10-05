165609b109034e86a3156d35d6e3e48def17aa02
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-05 16:14:38 +0800

    style: fix indent of utils/tac/tacvisitor.py

--- utils/tac/tacvisitor.py
+++ utils/tac/tacvisitor.py
@@ -4,32 +4,32 @@ from .tacinstr import *
 
 
 class TACVisitor:
-   def visitOther(self, instr: TACInstr) -> None:
+    def visitOther(self, instr: TACInstr) -> None:
         pass
 
-   def visitAssign(self, instr: Assign) -> None:
+    def visitAssign(self, instr: Assign) -> None:
         self.visitOther(instr)
 
-   def visitLoadImm4(self, instr: LoadImm4) -> None:
+    def visitLoadImm4(self, instr: LoadImm4) -> None:
         self.visitOther(instr)
 
-   def visitUnary(self, instr: Unary) -> None:
+    def visitUnary(self, instr: Unary) -> None:
         self.visitOther(instr)
 
-   def visitBinary(self, instr: Binary) -> None:
+    def visitBinary(self, instr: Binary) -> None:
         self.visitOther(instr)
 
-   def visitBranch(self, instr: Branch) -> None:
+    def visitBranch(self, instr: Branch) -> None:
         self.visitOther(instr)
 
-   def visitCondBranch(self, instr: CondBranch) -> None:
+    def visitCondBranch(self, instr: CondBranch) -> None:
         self.visitOther(instr)
 
-   def visitReturn(self, instr: Return) -> None:
+    def visitReturn(self, instr: Return) -> None:
         self.visitOther(instr)
 
-   def visitMemo(self, instr: Memo) -> None:
+    def visitMemo(self, instr: Memo) -> None:
         self.visitOther(instr)
 
-   def visitMark(self, instr: Mark) -> None:
+    def visitMark(self, instr: Mark) -> None:
         self.visitOther(instr)
